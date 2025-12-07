8c275819307533f5c6744d7ec4857ec11229cea3
Tobi Saputra <tobisaputra11@gmail.com>
2025-12-07 11:23:24 +0700

    chore: update package name to com.tobz.lmsunindra and app name to LMS Unindra

--- android/app/src/main/kotlin/com/example/lms_unindra/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lms_unindra
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/tobz/lmsunindra/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tobz.lmsunindra
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
